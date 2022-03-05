21cac7187c3813f428fd3d689007a1d732b625a1
MadameCebular <agata717@op.pl>
2022-03-05 15:16:44 +0100

    Calosc

---
+++ Main.py
@@ -0,0 +1,13 @@
+from zad3_1 import *
+from zad3_1 import *
+from zad3_1 import *
+
+pensja = int(input('Podaj pensję: '))
+wiek = int(input('Podaj wiek: '))
+kwota = int(input('Podaj kwotę: '))
+kurs = float(input('Podaj kurs: '))
+
+zad3_2(wiek, pensja)
+zad3_3(zad3_1(kwota, kurs))
+
+wartosc = zad3_2(wiek, pensja)
